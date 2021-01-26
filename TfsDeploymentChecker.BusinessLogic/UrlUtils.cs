cc3438b45da8cc90a79bb710cd8235db6f6fe7a2
MPI <mpierobon@sequel.com>
2021-01-26 15:27:55 +0000

    swapped stylecope for .editorconfig

--- TfsDeploymentChecker.BusinessLogic/UrlUtils.cs
+++ TfsDeploymentChecker.BusinessLogic/UrlUtils.cs
@@ -1,50 +1,32 @@
-﻿using TfsDeploymentChecker.Models.Tfs;
+using TfsDeploymentChecker.Models.Tfs;
 
 namespace TfsDeploymentChecker.BusinessLogic
 {
     public static class UrlUtils
     {
-        public static string GetReleaseDefinitionUrl (string tfsUrl, string tfsTeamProjectName, int releaseDefinitionId, string tfsApiVersion)
+		public static string GetReleaseDefinitionUrl(string tfsUrl, string tfsTeamProjectName, int releaseDefinitionId, string tfsApiVersion) => $"{tfsUrl}/{tfsTeamProjectName}/_apis/release/definitions/{releaseDefinitionId}?api-version={tfsApiVersion}";
+
+		public static string GetProjectDefinitionUrl(string tfsUrl, string tfsTeamProjectName, string tfsApiVersion) => $"{tfsUrl}/_apis/projects/{tfsTeamProjectName}?api-version={tfsApiVersion}";
+
+		public static string GetProjectCollectionsUrl(string tfsUrl, string tfsApiVersion) => $"{tfsUrl}/_apis/projects?api-version={tfsApiVersion}&top=1";
+
+		public static string GetReleasedEnvironmentUrl(string tfsUrl, string projectCollection, int releaseId, int environmentId, string queryOrder, string tfsApiVersion) => $"{tfsUrl}/{projectCollection}/_apis/release/deployments?definitionId={releaseId}&definitionEnvironmentId={environmentId}&queryOrder={queryOrder}&$top=1&api-version={tfsApiVersion}";
+
+		public static string GetTfsVersionControlChangesetUrl (string tfsUrl, string projectCollection, Definitionreference definitionReference)
         {
-            return $"{tfsUrl}/{tfsTeamProjectName}/_apis/release/definitions/{releaseDefinitionId}?api-version={tfsApiVersion}";
-        }
+			return definitionReference.sourceVersion == null
+				? $"{tfsUrl}/{projectCollection}/_versionControl"
+				: $"{tfsUrl}/{projectCollection}/_versionControl/changeset/{definitionReference.sourceVersion.id}";
+		}
 
-        public static string GetProjectDefinitionUrl (string tfsUrl, string tfsTeamProjectName, string tfsApiVersion)
+		public static string GetGitCommitUrl (string tfsUrl, string projectCollection, Definitionreference definitionReference)
         {
-            return $"{tfsUrl}/_apis/projects/{tfsTeamProjectName}?api-version={tfsApiVersion}";
-        }
-
-        public static string GetProjectCollectionsUrl (string tfsUrl, string tfsApiVersion)
-        {
-            return $"{tfsUrl}/_apis/projects?api-version={tfsApiVersion}&top=1";
-        }
-
-        public static string GetReleasedEnvironmentUrl (string tfsUrl, string projectCollection, int releaseId, int environmentId, string queryOrder, string tfsApiVersion)
-        {
-            return $"{tfsUrl}/{projectCollection}/_apis/release/deployments?definitionId={releaseId}&definitionEnvironmentId={environmentId}&queryOrder={queryOrder}&$top=1&api-version={tfsApiVersion}";
-        }
-
-        public static string GetTfsVersionControlChangesetUrl (string tfsUrl, string projectCollection, Definitionreference definitionReference)
-        {
-            if (definitionReference.sourceVersion == null)
-            {
-                return $"{tfsUrl}/{projectCollection}/_versionControl";
-            }
-
-            return $"{tfsUrl}/{projectCollection}/_versionControl/changeset/{definitionReference.sourceVersion.id}";
-        }
-
-        public static string GetGitCommitUrl (string tfsUrl, string projectCollection, Definitionreference definitionReference)
-        {
-            if (definitionReference.sourceVersion == null)
-            { 
-                return $"{tfsUrl}/_git/{projectCollection}?" +
-                    $"version=GB{definitionReference.branch.name.Replace(@"refs/heads/", string.Empty)}";
-            }
-
-            return $"{tfsUrl}/_git/{projectCollection}/commit/" +
-                                $"{definitionReference.sourceVersion.id}" +
-                                $"?refName={definitionReference.branch.name}";
-        }
-    }
+			return definitionReference.sourceVersion == null
+				? $"{tfsUrl}/_git/{projectCollection}?" +
+					$"version=GB{definitionReference.branch.name.Replace(@"refs/heads/", string.Empty)}"
+				: $"{tfsUrl}/_git/{projectCollection}/commit/" +
+								$"{definitionReference.sourceVersion.id}" +
+								$"?refName={definitionReference.branch.name}";
+		}
+	}
 }
